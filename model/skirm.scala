b3ba570b828734175324fbe25a1c6aa596cf2dee
Tate Bowers <dracoranger@hotmail.com>
2017-01-26 14:00:03 -0500

    V0.0.2
    Model compiles, refractored modifiers to be modifiers and modifier

--- model/skirm.scala
+++ model/skirm.scala
@@ -88,7 +88,7 @@ class World(size:Int, typ:Int)
         mi+= inc
         ma+= inc
       }
-      ret(i/size,i%size)=new Hex(rand.nextInt(mi,ma),0,i/size,i%size)
+      ret(i/size)(i%size)=new Hex(rand.nextInt(ma)+mi,0,new Empt(),i/size,i%size)
     }
     ret
   }
@@ -135,18 +135,21 @@ class Fight(a:Actor,b:Actor)
   var addMod=0.0
   var damMultMod=0.0
   var damAddMod=0.0
-  def getBaseHitChance(at:Actor,be:Actor):Double=at.getEmpathy(be)*(at.getHitAbil()-be.getDodgeAbil())
-  def getHitDamage(a:Actor,b:Actor)=a.getWeaponDam()//-b.getArmorRes()//TODO Implement armor
+  //Not drawn from here, i think
+  //def getBaseHitChance(at:Actor,be:Actor):Double=at.getEmpathy(be)*(at.getHitAbil-be.getDodgeAbil)
+  def getHitDamage(a:Actor,b:Actor)=a.getWeaponDam//-b.getArmorRes()//TODO Implement armor
   def getMods(s:State)=
   {
-    multMod=s.getMultMods(attac)
-    addMod=s.getAddMods(attac)
-    damMultMod=s.getDamMult(attac)
-    damAddMod=s.getDamAdd(attac)
+    //Done in Modifiers
+    ???
+    //multMod=s.getMultMods(attac)
+    //addMod=s.getAddMods(attac)
+    //damMultMod=s.getDamMult(attac)
+    //damAddMod=s.getDamAdd(attac)
   }
   def calculateHit():Double=
   {
-    var ret= multMod * getBaseHitChance(attac, defen) +addMod
+    var ret= multMod //* getBaseHitChance(attac, defen) +addMod
     ret
   }
   def doesHit():Boolean= if(calculateHit()>roll) true else false
@@ -173,33 +176,85 @@ class Move(a:Actor){
 ???
 }
 
-class Modifiers(state:State){
+class Modifier(in:Int){
+  val n=Array("Lunge","Parry","Blocked","Broken Arm","Exausted","Stunned","Concussed","Blinded","Bloodloss","Weap","Movem","Sprinting","Adrenaline","Angry","Broken Leg","Bloodloss","Slowed","Hunger","Thirst","Mana","Entropy")
+  val ap=Array(-.1, 0.25, -0.40,-0.25,  -0.1, -0.4, -0.5, -0.75, 0.0, 0.0, 0.0, 0.0, 0.0,  0.0,  0.0,    0.0,  0.0,  0.0,  0.0,   0.0, 0.0)
+  val an=Array(0.0, 0.0,  0.0,  0.0,    0.0,  0.0,  0.0,  0.0,   0.0, 0.0, 0.0, 0.0, 0.0,  0.0,  0.0,    0.0,  0.0,  0.0,  0.0,   0.0, 0.0)
+  val dp=Array(.25, 0.0,  0.0,  -0.25,  -0.1, 0.0,  0.0,  0.0,   0.0, 0.0, 0.0, 0.0, 0.0,  0.0,  0.0,    0.0,  0.0,  0.0,  0.0,   0.0, 0.0)
+  val dn=Array(0.0, 0.0,  0.0,  0.0,    0.0,  0.0,  0.0,  0.0,   0.0, 0.0, 0.0, 0.0, 0.0,  0.0,  0.0,    0.0,  0.0,  0.0,  0.0,   0.0, 0.0)
+  val en=Array(false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false)
+
+  var name=n(in)
+  var accPerc=ap(in)
+  var accNum=an(in)
+  var damPerc=dp(in)
+  var damNum=dn(in)
+  var enabled=en(in)
+
+  def getName()=name
+  def getAccPerc()=accPerc
+  def getAccNum()=accNum
+  def getDamPerc()=damPerc
+  def getDamNum()=damNum
+  def getEnabled()=enabled
+
+  def updateBloodLoss(loss:Int):Unit={
+    if(in==8){
+      if(loss>90) {
+        accPerc= -.9
+        enabled= true
+      }
+      else if(loss>80){
+        accPerc= -.75
+        enabled= true
+      }
+      else if(loss>70){
+        accPerc= -.6
+        enabled= true
+      }
+      else if(loss>60){
+        accPerc= -.45
+        enabled= true
+      }
+      else if(loss>50){
+        accPerc= -.3
+        enabled= true
+      }
+      else if(loss>40){
+        accPerc= -.2
+        enabled= true
+      }
+      else if(loss>30){
+        accPerc= -.15
+        enabled= true
+      }
+      else if(loss>20){
+        accPerc= -.1
+        enabled= true
+      }
+      else{
+        accPerc= 0.0
+        enabled= false
+      }
+    }
+  }
+
+}
+
+class Modifiers(){
   //accur perc,accur num, dam perc, dam num
-  val mods=Array(("Lunge",.1,0.0,.25,0.0,false),("Parry",0.25,0.0,0.0,0.0,false),("Blocked",-0.40,0.0,0.0,0.0,false),
-  ("Broken Arm",-0.25,0.0,-0.25,0.0,false),("Exausted",-0.1,0.0,-0.1,0.0,false),
-  ("Stunned",-0.4,0.0,0.0,0.0,false),("Concussed",-0.5,0.0,0.0,0.0,false),("Blinded",-0.75,0.0,0.0,0.0,false),
-  ("Bloodloss",0.0,0.0,0.0,0.0,false),("Weap",0.0,0.0,0.0,0.0,false),("Movem",0.0,0.0,0.0,0.0,false))
-  val specials=Array(("Sprinting",false),("Adrenaline",false), ("Angry",false),("Broken Leg",false),("Bloodloss",false),("Slowed",false))
-  val mun=Array(("Hunger",false),("Thirst",false))
-  val mag=Array(("Mana",false),("Entropy",false))
+  val mods=Array(new Modifier(0),new Modifier(1),new Modifier(2),new Modifier(3),new Modifier(4),new Modifier(5),new Modifier(6),new Modifier(7),new Modifier(8))
+  val specials=Array(new Modifier(9),new Modifier(10),new Modifier(11),new Modifier(12),new Modifier(13),new Modifier(14),new Modifier(15),new Modifier(16))
+  val mun=Array(new Modifier(17),new Modifier(18))
+  val mag=Array(new Modifier(19),new Modifier(20))
 
   //bloodloss
   var leakRate=0
   var loss=0
   //stores where the boolean is for mods and specials
-  final val EnabMod=6
-  final val EnabSpec=2
+  final val enabMod=6
+  final val enabSpec=2
   def incrimentBloodLoss():Unit=loss+=leakRate
-  def calculateBloodLoss():(String,Double,Double,Double,Double,Boolean)={
-    if(loss>90) ("Bloodloss",-0.9,0.0,0.0,0.0,mods(9).EnabMod)
-    else if(loss>80) ("Bloodloss",-0.75,0.0,0.0,0.0,true)
-    else if(loss>70) ("Bloodloss",-0.60,0.0,0.0,0.0,true)
-    else if(loss>60) ("Bloodloss",-0.45,0.0,0.0,0.0,true)
-    else if(loss>50) ("Bloodloss",-0.30,0.0,0.0,0.0,true)
-    else if(loss>40) ("Bloodloss",-0.20,0.0,0.0,0.0,true)
-    else if(loss>30) ("Bloodloss",-0.15,0.0,0.0,0.0,true)
-    else if(loss>20) ("Bloodloss",-0.10,0.0,0.0,0.0,true)
-  }
   def modBloodLoss():Int={
     if(loss>90) 1
     else if(loss>80) 2
@@ -209,52 +264,58 @@ class Modifiers(state:State){
     else if(loss>40) 6
     else if(loss>30) 7
     else if(loss>20) 8
-    else if(loss>10&&specials(2)._EnabSpec) 11
-    else if(specials(5).EnabSpec&&specials(2).EnabSpec) 12
-    else if(loss>10&&specials(6)._2) 13
-    else if(specials(5).EnabSpec&&specials(6).EnabSpec) 14
+    else if(loss>10&&specials(2).getEnabled()) 11
+    else if(specials(5).getEnabled()&&specials(2).getEnabled()) 12
+    else if(loss>10&&specials(6).getEnabled()) 13
+    else if(specials(5).getEnabled()&&specials(6).getEnabled()) 14
     else if(loss>10) 9
-    else if(specials(5).EnabSpec) 10
+    else if(specials(5).getEnabled()) 10
     else 0
   }
 
   def update():Unit={
-    mods(8)=calculateBloodLoss()
-    mods(9)=state.calculateWeap()
-    mods(10)=state.calculateMovem()
+    mods(8).updateBloodLoss(loss)
+    //mods(9)=state.calculateWeap()
+    //mods(10)=state.calculateMovem()
   }
 
-  def totaledMods():Double={
+  def totaledMods():Array[Double]=
+  {
     //neg(accur perc,accur num, dam perc, dam num)pos(accur perc,accur num, dam perc, dam num)
     //arrays 0
     // tuple ._1
     update()
-    var stor=(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
-
-    for(i<-mods){
-      if(i._EnabMod){
-        for(j<-2 to i.length-1){
-          if (i._j<0){
-            var temp=i._j
-            var locl=j-2
-            if(specials(1)._EnabSpec) temp=temp*.5
-            if(specials(2)._EnabSpec) temp=temp*.25
-            stor._locl +=temp*.25
-
-          }
-          else(i._j>0){
-            var locl=j+4-2
-            stor._locl +=i._j
-          }
+    var stor=Array(1.0,0.0,1.0,0.0)//,0.0,0.0,0.0,0.0)
+    var num=0
+    for(i<-mods)
+    {
+      if(i.getEnabled())
+      {//TODO: use for comprihensions
+        stor(0) *=
+        {
+          var temp=i.getAccPerc
+          if(i.getAccPerc()<0&&specials(12).getEnabled) temp=.5*temp
+          if(i.getAccPerc()<0&&specials(13).getEnabled) temp=.25*temp
+          temp
+        }
+        stor(1) +=
+        {
+          i.getAccNum()
+        }
+        stor(2) *=
+        {
+          var temp=i.getDamPerc
+          if(i.getDamPerc()<0&&specials(12).getEnabled) temp=.5*temp
+          if(i.getDamPerc()<0&&specials(13).getEnabled) temp=.25*temp
+          temp
+        }
+        stor(3) +=
+        {
+          i.getDamNum()
         }
       }
     }
-    var ret=(0.0,0.0,0.0,0.0)
-    for(i<-0 to (stor.length-1)/2){
-      var add=((stor.length-1)/2)+i
-      ret._i+= stor._i + stor._add//Might need to be 1
-    }
-    ret
+    stor
   }
 }
 /*
@@ -265,11 +326,11 @@ class Modifiers(state:State){
 */
 
 class State(actor:Actor, target:Actor){
-  var force=0
-  var location=locl
-  val move=new Move(a:Actor)
+  //var force=0
+  //var location=
+  //val move=new Move(a:Actor)
 
-  val modif= new Modifiers()
+  //val modif= new Modifiers()
 }
 
 
@@ -277,5 +338,5 @@ class State(actor:Actor, target:Actor){
 TODO:completely redo
 */
 class Combat(a:Actor,b:Actor){
-  for(i<-actors) ???
+  //for(i<-actors) ???
 }
